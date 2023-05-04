e343e9eda6834a3467b3f838ebb19be15b412745
kimyen07 <97496482+kimyen07@users.noreply.github.com>
2023-05-04 16:36:05 +0700

    Add files via upload

---
+++ MiddleWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Karaokay
+{
+    /// <summary>
+    /// Interaction logic for MiddleWindow.xaml
+    /// </summary>
+    public partial class MiddleWindow : Window
+    {
+        public MiddleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void adminBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
